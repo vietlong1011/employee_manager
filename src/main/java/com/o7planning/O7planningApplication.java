628c51b687cb46a871242ead4d54645228cfb613
vietlong1011 <longlieuliny@gmail.com>
2023-04-21 09:20:46 +0700

    commit first

---
+++ src/main/java/com/o7planning/O7planningApplication.java
@@ -0,0 +1,14 @@
+package com.o7planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O7planningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O7planningApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/o7planning/O7planningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o7planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O7planningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
